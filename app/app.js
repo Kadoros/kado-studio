2d331e308157ed9385a43828dabb90c30745ab1c
kado1130 <kadoros1130@gmail.com>
2024-05-11 17:15:04 +0200

    add js to HTML

--- app/app.js
+++ app/app.js
@@ -13,8 +13,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-
+//middle ware
 app.use("/", home);
+app.use(express.static(`${__dirname}/src/public`));
 
 module.exports = app;
 
